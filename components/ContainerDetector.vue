d796253c1e63c47b180eb50d3e9f20007ac2e95d
Jonathan Law <jon_law98@hotmail.com>
2024-01-13 23:47:41 +0800

    Testing running container detection before sending to API

--- components/ContainerDetector.vue
+++ components/ContainerDetector.vue
@@ -5,10 +5,11 @@
       Launch CNRAI
     </v-btn>
 
-    <v-overlay v-model="overlay" width="100%" height="100%" :style="'background-color: ' + dynamic_bg_color" style="opacity: 1">
+    <v-overlay v-model="overlay" width="100%" height="100%" :style="'background-color: ' + dynamic_bg_color"
+               style="opacity: 1">
       <v-container style="height: 100vh">
         <v-btn icon="mdi-close" size="default" class="close-btn" @click="overlay = false"></v-btn>
-        <v-row class="mt-12">
+        <v-row>
           <v-col cols="12" md="8" offset-md="2">
             <v-card rounded="lg">
               <v-card-item>
@@ -31,9 +32,21 @@
                   <v-col cols="12" md="8">
                     <v-card variant="outlined" height="280" style="overflow-y: scroll">
                       <v-card-item>
-                        <pre style="overflow-x: auto; word-wrap: break-word; white-space: pre-wrap; font-size: 14px;">{{ output }}</pre>
+                        <pre style="overflow-x: auto; word-wrap: break-word; white-space: pre-wrap; font-size: 14px;">{{
+                            output
+                          }}</pre>
                       </v-card-item>
                     </v-card>
+                  </v-col>
+
+                  <v-col cols="12">
+                    <v-switch
+                        color="primary"
+                        inset
+                        v-model="allow_local_detection"
+                        hide-details
+                        label="container detection"
+                    ></v-switch>
                   </v-col>
 
                   <v-col cols="12" md="10" lg="8">
@@ -61,6 +74,10 @@
 </template>
 
 <script>
+import * as tf from '@tensorflow/tfjs';
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
+import {toRaw} from 'vue'
+
 export default {
   name: "ContainerDetector",
   data: () => ({
@@ -68,20 +85,77 @@ export default {
     input_image: [],
     display_image: null,
     final_display_image: '/img/upload_container_image.webp',
-    output: JSON.stringify({'message': 'upload a shipping container image'}, null, '\t'),
-    dynamic_bg_color: '#d8c3a5'
+    output: JSON.stringify({'message': 'Loading model...'}, null, '\t'),
+    dynamic_bg_color: '#d8c3a5',
+    allow_local_detection: true,
+    model: null,
+    local_detection: [],
   }),
   methods: {
-    output_message(msg) {
-      return JSON.stringify({'message': msg}, null, '\t')
+    set_output_message(msg) {
+      this.output = JSON.stringify({'message': msg}, null, '\t')
+    },
+    async get_container_door(url) {
+      this.set_output_message('checking for container...')
+      this.local_detection = []
+
+      let img = new Image();
+      img.src = url
+
+      // Wait for image to be initialized
+      const waitImgLoad = async (img) => {
+        return new Promise((resolve) => {
+          img.onload = async () => {
+            resolve(true)
+          }
+        })
+      }
+      let imgLoaded = await waitImgLoad(img)
+
+      // get detection result
+      const rawModel = toRaw(this.model)
+      const tfImg = tf.browser.fromPixels(img).toInt();
+      const expandedDims = tfImg.expandDims();
+
+      await rawModel.executeAsync(expandedDims).then(predictions => {
+        this.set_output_message('prediction completed, parsing...')
+        // for (let i = 0; i < predictions.length; i++) {
+        //   console.log(i, predictions[i].arraySync()[0])
+        // }
+
+        let detection_res = []
+        for (let i = 0; i < predictions[0].arraySync()[0].length; i++) {
+          if (predictions[3].arraySync()[0][i] > 0.8) {
+            detection_res.push({
+              bbox: predictions[1].arraySync()[0][i],
+              probability: predictions[3].arraySync()[0][i]
+            })
+          }
+        }
+
+        this.local_detection = detection_res
+
+        // 0 is classes
+        // 1 is bounding box
+        // 3 is probability
+      })
+
+          .catch((err) => {
+            this.set_output_message(err)
+          })
+
+          .finally(() => {
+            this.set_output_message(this.local_detection)
+            tf.dispose([tfImg, expandedDims])
+          })
     },
     async detect_container_number() {
-      let tbp = this.input_image[0]
+      const tbp /** @type{File} */ = this.input_image[0]
       this.final_display_image = null
 
       if (!tbp || !tbp.type.startsWith('image')) {
         this.display_image = null
-        this.output = !tbp ? this.output_message('no file found') : this.output_message('wrong file type')
+        !tbp ? this.set_output_message('no file found') : this.set_output_message('wrong file type')
 
         this.final_display_image = '/img/upload_container_image.webp'
 
@@ -90,14 +164,35 @@ export default {
 
       this.display_image = URL.createObjectURL(tbp)
 
+      const blobToData = async (blob) => {
+        return new Promise((resolve) => {
+          const reader = new FileReader()
+          reader.onloadend = () => resolve(reader.result)
+          reader.readAsDataURL(blob)
+        })
+      }
+
+      // If we allow local SSD to run and provide output
+      if (this.allow_local_detection) {
+        let res = await blobToData(tbp)
+
+        await this.get_container_door(res)
+        if (this.local_detection.length === 0) {
+          this.set_output_message('no containers detected. this could be due to image quality or container being too far.')
+          this.final_display_image = this.display_image
+          return false
+        }
+      }
+
       const formData = new FormData()
       formData.append('image', tbp)
+      formData.append('bbox', this.local_detection ?? [])
 
-      this.output = this.output_message('detecting...')
+      this.set_output_message('sending for ocr...')
 
       const config = useRuntimeConfig()
       if (!config.public.ctnrApiUrl) {
-        this.output = this.output_message('issue calling backend, please inform the administrator.')
+        this.set_output_message('issue calling backend, please inform the administrator.')
         return false
       }
 
@@ -113,13 +208,17 @@ export default {
             }
           })
           .catch(error => {
-            this.output = JSON.stringify(error, null, '\t')
+            this.set_output_message(error)
           })
           .finally(() => {
             this.final_display_image = this.display_image
           })
       return true
     }
+  },
+  async mounted() {
+    this.model = await loadGraphModel("/model/model.json")
+    this.set_output_message('upload a shipping container image')
   }
 }
 </script>
